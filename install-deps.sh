8f0b279c21dd064e7dd7deb807ffa010e41f6cc2
hcoffey1 <hcoffey1@vols.utk.edu>
2021-10-18 23:25:23 -0500

    Adds missing deps

--- install-deps.sh
+++ install-deps.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/sudo /bin/bash
 
-apt install -y libevent-dev llvm-8-dev llvm-8 llvm-8-doc python3
+apt install -y libevent-dev llvm-8-dev llvm-8 llvm-8-doc python3 libjemalloc-dev libtbb-dev
